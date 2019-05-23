bd46f8565d8e17200b5dc33fe6c75b8923eed662
Kristof <kristof.neyt@ugent.be>
2019-05-23 10:46:54 +0200

    (Hopefully) Fix photo path on linux & windows

--- app/server/routes.js
+++ app/server/routes.js
@@ -762,7 +762,7 @@ module.exports = app => {
     });
 
     app.get("/photo/:id", (req, res) => {
-        res.sendFile(path.join(data + '/photo/' + req.params.id));
+        res.sendFile(path.normalize(__dirname + "/../../" + data + '/photo/' + req.params.id));
     });
 
     app.delete("/photo/:id", function (req, res) {
