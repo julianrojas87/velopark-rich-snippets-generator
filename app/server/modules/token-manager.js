7f590629c6b5c26e8af19652b6da75d5650f5afe
Julian Rojas <julian.rojas87@gmail.com>
2021-03-21 16:33:25 +0100

    Completely remove reliance on config_secret.json

--- app/server/modules/token-manager.js
+++ app/server/modules/token-manager.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const config_secret = JSON.parse(fs.readFileSync('./config_secret.json', 'utf-8'));
 
 exports.getStringForToken = function (token, callback) {
     if (!token)
         callback("No token found");
     else {
-        jwt.verify(token, config_secret.tokenSecret, function (err, decoded) {
+        jwt.verify(token, process.env.SECRET_TOKEN, function (err, decoded) {
             if (err) {
                 callback(err);
             } else {
@@ -17,7 +16,7 @@ exports.getStringForToken = function (token, callback) {
 };
 
 exports.getTokenForString = function (username) {
-    let token = jwt.sign({name: username}, config_secret.tokenSecret, {
+    let token = jwt.sign({name: username}, process.env.SECRET_TOKEN, {
         expiresIn: 24 * 60 * 60 * 1000 // 1 day
     });
     return token;
